2396d742df7877ef4c6ec177ac798e58c6f932c2
natif <warrior1990@gmail.com>
2016-02-07 20:44:49 +0100

    Renaming variables

--- gitrepostats.py
+++ gitrepostats.py
@@ -4,42 +4,41 @@ import subprocess
 
 display_users_cmd = "git log --pretty=format:\" %an \""
 
-def execute(command):    
+def run_command(command):    
   popen = subprocess.Popen(command, stdout=subprocess.PIPE)
   lines_iterator = iter(popen.stdout.readline, b"")
   return lines_iterator;
 
-users = []
+commiters_list = []
+commiters_iterator = run_command(display_users_cmd)
 
-users_iterator = execute(display_users_cmd)
+for line in commiters_iterator:
+	commiters_list.append(line.strip().decode('utf-8'))
 
-for line in users_iterator:
-	users.append(line.strip().decode('utf-8'))
+commiters = set(commiters_list)
+lines_added_by_commiter = {}
+lines_deleted_by_commiter = {}
 
-users_set = set(users)
-users_dict_added = {}
-users_dict_deleted = {}
-
-for user in users_set:
-	file_lines = execute("git log --author=\""+user+"\" --pretty=tformat: --numstat")
+for commiter in commiters:
+	commit_lines = run_command("git log --author=\""+commiter+"\" --pretty=tformat: --numstat")
 	lines_added = 0
 	lines_deleted = 0 
-	for fline in file_lines:
-		file_list = fline.split()
-		if file_list:
-			lines_added += int(file_list[0])
-			lines_deleted += int(file_list[1])
+	for commit_line in commit_lines:
+		line_result = commit_line.split()
+		if line_result:
+			lines_added += int(line_result[0])
+			lines_deleted += int(line_result[1])
 
-	users_dict_added[user] = lines_added
-	users_dict_deleted[user] = lines_deleted
+	lines_added_by_commiter[commiter] = lines_added
+	lines_deleted_by_commiter[commiter] = lines_deleted
 
-users_set = sorted(users_set)
+commiters = sorted(commiters)
 
-for commiter in users_set:
+for commiter in commiters:
 	print(commiter)
-	print("Lines added: " + str(users_dict_added[commiter]))
-	print("Lines deleted: " + str(users_dict_deleted[commiter]))
-	subtraction = users_dict_added[commiter] - users_dict_deleted[commiter]
+	print("Lines added: " + str(lines_added_by_commiter[commiter]))
+	print("Lines deleted: " + str(lines_deleted_by_commiter[commiter]))
+	subtraction = lines_added_by_commiter[commiter] - lines_deleted_by_commiter[commiter]
 	print("Added-Deleted: " + str(subtraction))
 	print("\n")
 
